6228f143cdf0b86696f96d58e94fe1dfb67fbefe
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-13 00:19:22 -0500

    feat: add ESLint plugins and rules for import sorting and unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
@@ -15,6 +17,31 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^@?\\w'],
+            ['^@/'],
+            ['^\\.'],
+            ['\\.css$', '\\.scss$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'never'],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
