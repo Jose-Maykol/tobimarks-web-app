8d1ce7236aa7d906f7b277b17ee7d592b8934e39
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-13 23:52:07 -0500

    feat: integrate Google OAuth provider with client ID from environment variables

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from 'react-router'
 import { HeroUIProvider } from '@heroui/react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import { GOOGLE_CLIENT_ID } from './core/config/env'
+
 const queryClient = new QueryClient()
+const clientId = GOOGLE_CLIENT_ID
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <HeroUIProvider>{children}</HeroUIProvider>
+        <HeroUIProvider>
+          <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>
+        </HeroUIProvider>
       </QueryClientProvider>
     </BrowserRouter>
   )
