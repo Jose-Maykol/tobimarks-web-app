7e7c453cca524d50d44cc6df6d2f68045d5688c1
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-08-28 07:57:28 -0500

    feat: enhance login page layout with background gradients

--- src/features/auth/pages/login/components/LoginPage.tsx
+++ src/features/auth/pages/login/components/LoginPage.tsx
@@ -18,13 +18,28 @@ const LoginPage = (): JSX.Element => {
   }
 
   return (
-    <div>
-      <h1>Login</h1>
-      <div>
-        <GoogleLogin
-          onSuccess={handleLoginSuccess as (res: unknown) => void}
-          onError={() => console.error('Error de inicio de sesión')}
-        />
+    <div className='min-h-screen p-4 relative overflow-hidden'>
+      <div className='absolute -top-24 -left-36 h-96 w-96 bg-gradient-to-br from-indigo-600 via-indigo-500 to-purple-500 rounded-full filter blur-3xl opacity-70'></div>
+      <div className='absolute top-[calc(50%-260px)] left-[calc(50%-260px)] h-[520px] w-[520px] bg-gradient-to-br from-blue-700 to-purple-500 rounded-full filter blur-3xl opacity-40'></div>
+      <div className='absolute -bottom-24 -right-36 h-96 w-96 bg-gradient-to-br from-indigo-600 via-indigo-500 to-purple-500 rounded-full filter blur-3xl opacity-70'></div>
+
+      <div className='relative flex justify-center items-center flex-col h-screen gap-2'>
+        <h1 className='text-neutral-100 text-5xl font-bold text-center'>
+          Tus bookmarks están a un clic de distancia, <br />{' '}
+          <span className='bg-linear-to-r from-violet-500 to-blue-500 bg-clip-text text-6xl text-transparent'>
+            Tobimarks
+          </span>
+        </h1>
+        <p className='text-neutral-300 text-center max-w-2xl text-xl font-semibold'>
+          El gestor de bookmarks inteligente que convierte el caos de tus enlaces en una biblioteca
+          organizada y accesible al instante.
+        </p>
+        <div>
+          <GoogleLogin
+            onSuccess={handleLoginSuccess as (res: unknown) => void}
+            onError={() => console.error('Error de inicio de sesión')}
+          />
+        </div>
       </div>
     </div>
   )
