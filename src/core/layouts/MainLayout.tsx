83a2889d4641810a3cd7c86335350d4dcfe78982
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-10-06 23:58:03 -0500

    feat: implement tag fetching logic in main layout component

--- src/core/layouts/MainLayout.tsx
+++ src/core/layouts/MainLayout.tsx
@@ -1,10 +1,22 @@
-import type { JSX } from 'react'
+import { type JSX, useEffect } from 'react'
 import { Outlet } from 'react-router'
 
+import { useTagStore } from '../../features/tags/stores/useTagStore'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 
 const MainLayout = (): JSX.Element => {
+  //TODO: Usar react-query para manejar el estado de los tags
+  const tags = useTagStore((state) => state.tags)
+  const getList = useTagStore((state) => state.getList)
+
+  useEffect(() => {
+    if (tags.length === 0) {
+      console.log('Fetching tags...')
+      getList()
+    }
+  }, [getList, tags.length])
+
   return (
     <div className='min-h-screen bg-background w-full'>
       <Header />
